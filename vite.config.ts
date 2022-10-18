39f1d1dc9112acc19c21de7a52b31e13e9ec3ca4
OceanPresent <oceanpresent@163.com>
2022-10-18 10:30:07 +0800

    deploy: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -33,20 +33,21 @@ export default defineConfig({
     open: true,
   },
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: '[[name]]',
-      fileName: format => `peintre.${format}.js`,
-    },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
+    // lib: {
+    //   entry: path.resolve(__dirname, 'src/index.ts'),
+    //   name: 'webcam-minesweeper',
+    //   fileName: format => `peintre.${format}.js`,
+    // },
+    // rollupOptions: {
+    //   // 确保外部化处理那些你不想打包进库的依赖
+    //   external: ['vue'],
+    //   output: {
+    //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+    //     globals: {
+    //       vue: 'Vue',
+    //     },
+    //   },
+    // },
   },
+  base: '/Webcam-MineSweeper/',
 })
